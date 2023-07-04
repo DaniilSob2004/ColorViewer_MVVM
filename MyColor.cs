f3e0951e5729500ef035b9d1acc6f6190b478dc7
Daniil <Soboliev.danik@gmail.com>
2023-07-04 13:00:30 +0300

    Create project

---
+++ MyColor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorViewer_MVVM
+{
+    public class MyColor
+    {
+        private int alpha;
+        private int red;
+        private int green;
+        private int blue;
+
+        public MyColor(int alpha = 0, int red = 0, int green = 0, int blue = 0)
+        {
+            Alpha = alpha;
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public int Alpha
+        {
+            get => alpha;
+            set => alpha = value;
+        }
+
+        public int Red
+        {
+            get => red;
+            set => red = value;
+        }
+
+        public int Green
+        {
+            get => green;
+            set => green = value;
+        }
+
+        public int Blue
+        {
+            get => blue;
+            set => blue = value;
+        }
+    }
+}
---
+++ ViewModelColor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace ColorViewer_MVVM
+{
+    public class ViewModelColor : INotifyPropertyChanged
+    {
+        private MyColor color;
+        private Color backgroundColor;
+
+        public ViewModelColor()
+        {
+            color = new MyColor();
+        }
+
+        public int AlphaChannel
+        {
+            get => color.Alpha;
+            set
+            {
+                color.Alpha = value;
+                OnPropertyChanged("AlphaChannel");
+                UpdateBackgroundColor();
+            }
+        }
+
+        public int RedChannel
+        {
+            get => color.Red;
+            set
+            {
+                color.Red = value;
+                OnPropertyChanged("RedChannel");
+                UpdateBackgroundColor();
+            }
+        }
+
+        public int GreenChannel
+        {
+            get => color.Green;
+            set
+            {
+                color.Green = value;
+                OnPropertyChanged("GreenChannel");
+                UpdateBackgroundColor();
+            }
+        }
+
+        public int BlueChannel
+        {
+            get => color.Blue;
+            set
+            {
+                color.Blue = value;
+                OnPropertyChanged("BlueChannel");
+                UpdateBackgroundColor();
+            }
+        }
+
+        public Color BackgroundColor
+        {
+            get => backgroundColor;
+            set
+            {
+                backgroundColor = value;
+                OnPropertyChanged("BackgroundColor");
+            }
+        }
+
+
+        private void UpdateBackgroundColor()
+        {
+            BackgroundColor = Color.FromArgb((byte)AlphaChannel, (byte)RedChannel, (byte)GreenChannel, (byte)BlueChannel);
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+    }
+}
